81c4cc59497353aed30b6eca5d9cf7efc2d8dd73
awet melake <awetmelake@gmail.com>
2019-06-08 20:42:13 -0500

    removed local toggle function moved to Toggle class

--- src/components/CreateTask.js
+++ src/components/CreateTask.js
@@ -1,16 +1,8 @@
 import React, { Component } from "react";
 
 class CreateTask extends Component {
-  state = {
-    on: false,
-    title: ""
-  };
-  toggle = () => {
-    this.setState({
-      on: !this.state.on
-    });
-  };
   onChange = e => {
+    // e.preventDefault();
     this.setState({
       [e.target.name]: e.target.value
     });
@@ -18,7 +10,7 @@ class CreateTask extends Component {
   render() {
     return (
       <div>
-        {this.state.on && (
+
           <form style={promptStyle}>
             <label>Title</label>
             <br/>
@@ -32,29 +24,15 @@ class CreateTask extends Component {
               name="description"
               onChange={this.onChange}
               style={{padding: '10px',}}
-
             />
             <br/>
-            <button style={{}} type="submit">ok</button>
           </form>
-        )}
-        <p style={btnStyle} onClick={this.toggle}>
-          +
-        </p>
+
       </div>
     );
   }
 }
-const btnStyle = {
-  float: "right",
-  color: "green",
-  padding: "0 10px",
-  borderRadius: "100px",
-  position: "relative",
-  bottom: "33px",
-  fontSize: "30px",
-  fontWeight: "1000",
-};
+
 const promptStyle = {
   textAlign: "center",
   padding: "20px",
