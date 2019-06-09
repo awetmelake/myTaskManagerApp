abac29592fb06329509369f49a474474c6e1aae3
awet melake <awetmelake@gmail.com>
2019-06-08 21:56:55 -0500

    functional submit method

--- src/components/CreateTask.js
+++ src/components/CreateTask.js
@@ -1,33 +1,40 @@
 import React, { Component } from "react";
 
 class CreateTask extends Component {
-  onChange = e => {
-    // e.preventDefault();
-    this.setState({
-      [e.target.name]: e.target.value
-    });
+  state = {
+    title: '',
+    focused: false,
+    time: 0,
+    panel: this.props.panel,
   };
+  handleChange = e => {
+    this.setState({[e.target.name]: e.target.value});
+  };
+  handleClick = (e) => {
+    e.preventDefault();
+    if (this.state.title.length > 0) {
+      this.props.createTask(this.state);
+      this.setState({title: '', discription: ''}); //clear field
+    }
+    this.props.toggle();
+
+  }
   render() {
     return (
       <div>
-
-          <form style={promptStyle}>
+        <form style={promptStyle}>
+          <div style={{ marginTop: "20px" }}>
             <label>Title</label>
-            <br/>
-            <input
-              name="title"
-              onChange={this.onChange}
-              style={{padding: '5px',}}
-            />
+            <br />
+            <input name="title" onChange={this.handleChange} />
+          </div>
+          <div style={{ marginTop: "20px" }}>
             <label>Description</label>
-            <textarea
-              name="description"
-              onChange={this.onChange}
-              style={{padding: '10px',}}
-            />
-            <br/>
-          </form>
-
+            <br />
+            <textarea name="description" onChange={this.handleChange} />
+          </div>
+          <button style={{marginTop: '10px'}} type='submit' onClick={this.handleClick}>OK</button>
+        </form>
       </div>
     );
   }
@@ -35,14 +42,13 @@ class CreateTask extends Component {
 
 const promptStyle = {
   textAlign: "center",
-  padding: "20px",
   width: "300px",
   height: "200px",
   backgroundColor: "#f3f3f3",
   position: "fixed",
   top: "50vh",
   left: "calc(50vw - 150px)",
-  border: "2px solid black",
+  border: "2px solid black"
 };
 
 export default CreateTask;
