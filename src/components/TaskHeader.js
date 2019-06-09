7129a714c01126e640fa19ac57b516def659d0f0
awet melake <awetmelake@gmail.com>
2019-06-08 21:58:14 -0500

    passed in necessary props

--- src/components/TaskHeader.js
+++ src/components/TaskHeader.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { Component } from "react";
 import CreateTask from './CreateTask.js'
+import Toggle from './Toggle.js'
 
-const TaskHeader = (props) => (
-  <div className="task-header">
-    <h2>{props.title}</h2>
-    <CreateTask/>
-  </div>
-);
-
+class TaskHeader extends Component {
+  render(){
+    return(
+      <div className="task-header">
+        <h2>{this.props.panel}</h2>
+        <Toggle status={this.props.panel} createTask={this.props.createTask}>
+          {({on, toggle}) => (
+            <div>
+              {on && (<CreateTask toggle={toggle} panel={this.props.panel} createTask={this.props.createTask}/>)}
+              <p style={btnStyle} onClick={toggle}>+</p>
+            </div>
+          )}
+        </Toggle>
+      </div>
+    )
+  }
+}
+const btnStyle = {
+  float: "right",
+  color: "green",
+  padding: "0 10px",
+  borderRadius: "100px",
+  position: "relative",
+  bottom: "33px",
+  fontSize: "30px",
+  fontWeight: "1000",
+};
 
 export default TaskHeader;
