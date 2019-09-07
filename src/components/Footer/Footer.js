cc54cbbd5c09a8d7fb0a7a68dc91915667fea028
awet melake <awetmelake@gmail.com>
2019-09-07 18:11:51 -0500

    animation update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -18,35 +18,16 @@ class Footer extends Component {
         <div className="container">
           <div className="row">
             <div className="col l6 s12">
-              <p className="grey-text text-lighten-4" data-aos="fade-right">
-                Portfolio:
-                <a
-                  className="grey-text text-lighten-3"
-                  href="http://www.awettech.com"
-                >
-                  {" "}
-                  www.awettech.com
-                </a>
-              </p>
-              <p className="grey-text text-lighten-4" data-aos="fade-right">
-                Email: awetmelake@gmail.com
-              </p>
-              <p className="grey-text text-lighten-4" data-aos="fade-right">
-                View source:{" "}
-                <a href="https://github.com/awetmelake/myTaskManagerApp">github.com/awetmelake/myTaskManagerApp</a>
-              </p>
-            </div>
-            <div className="col l4 offset-l2 s12">
               <h5 className="white-text" data-aos="fade-in" data-aos-delay="0">
                 TaskFlow
               </h5>
               <ul>
-                <li data-aos="fade-left" data-aos-delay="0">
+                <li data-aos="fade-right" data-aos-delay="0">
                   <Link to="/">Home</Link>
                 </li>
 
                 {auth.uid ? (
-                  <li data-aos="fade-left" data-aos-delay="100">
+                  <li data-aos="fade-right" data-aos-delay="100">
                     <Link
                       className="grey-text text-lighten-3"
                       to="/user-boards"
@@ -56,21 +37,42 @@ class Footer extends Component {
                   </li>
                 ) : (
                   <>
-                    <li data-aos="fade-left" data-aos-delay="200">
+                    <li data-aos="fade-right" data-aos-delay="200">
                       <Link to="/log-in">Log in</Link>
                     </li>
-                    <li data-aos="fade-left" data-aos-delay="300">
+                    <li data-aos="fade-right" data-aos-delay="300">
                       <Link to="/sign-up">Sign up</Link>
                     </li>
                   </>
                 )}
               </ul>
             </div>
+            <div className="col l4 offset-l2 s12">
+              <p className="grey-text text-lighten-4" data-aos="fade-left" data-aos-delay="200">
+                Portfolio:
+                <a
+                  className="grey-text text-lighten-3"
+                  href="http://www.awettech.com"
+                >
+                  {" "}
+                  www.awettech.com
+                </a>
+              </p>
+              <p className="grey-text text-lighten-4" data-aos="fade-left" data-aos-delay="300">
+                Email: awetmelake@gmail.com
+              </p>
+              <p className="grey-text text-lighten-4" data-aos="fade-left" data-aos-delay="400">
+                View source:{" "}
+                <a href="https://github.com/awetmelake/myTaskManagerApp">
+                  github.com/awetmelake/myTaskManagerApp
+                </a>
+              </p>
+            </div>
           </div>
         </div>
         <div
           className="footer-copyright"
-          data-aos="fade-right"
+          data-aos="fade-left"
           data-aos-delay="200"
         >
           <div className="container">
